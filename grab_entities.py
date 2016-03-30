988e2ce3d3234d9a17e586ed2e897055966a7ca9
Huichao Xue <xue.huichao@gmail.com>
2016-03-29 21:28:14 -0700

    Added documentations onto grab_entities.py.

--- grab_entities.py
+++ grab_entities.py
@@ -1,6 +1,22 @@
+"""
+Grabbing Baidu Baike for entity definitions.
+
+This script grabs all entities in the entity directory, and put them into --db_loc.
+It grabs everything in a multi-threading fashion. The first time some fetching attmpts may fail,
+so we need several runs to guarantee every entity has some data.
+
+First time:
+  # python grab_entities.py --retry_on_failed=false
+
+You may then see how many entries were not fetched by:
+  # sqlite3 entities_db/entities.db 'select count(*) from search_results where failed'
+
+When the number is not 0, iteratively try for a couple of times:
+  # python grab_entities.py
+"""
+
 import os
 from os import path
-entity_dir = r'data/ENTITY SET'
 import sqlite3
 import threading
 import Queue
@@ -10,11 +26,14 @@ from bs4 import BeautifulSoup
 import gflags
 
 gflags.DEFINE_boolean("retry_on_failed", True, "When set, will only retry on failed entities.")
-gflags.DEFINE_string("db_loc", "entities_db/entities.db", "When set, will only retry on failed entities.")
+gflags.DEFINE_string("db_loc", "entities_db/entities.db", 
+                     "When set, will only retry on failed entities.")
 gflags.DEFINE_integer("n_fetching_threads", 500, "Number of threads to retrieve results.")
+gflags.DEFINE_string("entity_dir", "data/ENTITY SET", "Directory containing entities.")
 
 def GetAllEntities():
-    filenames = [path.join(entity_dir, fname) for fname in os.listdir(entity_dir)]
+    filenames = [path.join(gflags.FLAGS.entity_dir, fname) for fname in os.listdir(
+        gflags.FLAGS.entity_dir)]
     all_entities = []
     for fname in filenames:
         with open(fname) as infile:
@@ -63,7 +82,8 @@ class WritingThread(threading.Thread):
         conn = sqlite3.connect(self.db_loc)
         cursor = conn.cursor()
         if not self.modify:
-            cursor.execute('create table search_results(entity_name text, response text, failed bool)')
+            cursor.execute(
+                'create table search_results(entity_name text, response text, failed bool)')
             cursor.execute('create index ent_name on search_results(entity_name);')
             conn.commit()
 
@@ -77,10 +97,12 @@ class WritingThread(threading.Thread):
             entity_name, text, failed = writing_queue.get()
             
             if not self.modify or not EntityNameAlreadyOccur(entity_name):
-                cursor.execute('insert into search_results values (?, ?, ?)', (entity_name, text, failed))
+                cursor.execute(
+                    'insert into search_results values (?, ?, ?)', (entity_name, text, failed))
             else:
-                cursor.execute('update search_results set response=?,failed=? where entity_name=?', (
-                    text, failed, entity_name))
+                cursor.execute(
+                    'update search_results set response=?,failed=? where entity_name=?', (
+                        text, failed, entity_name))
             n_finished += 1
             if n_finished % 100 == 0:
                 print "finished %d, out of %d" % (n_finished, self.len_all_entities)
@@ -90,7 +112,8 @@ class WritingThread(threading.Thread):
 def GetFailedEntities(loc):
     conn = sqlite3.connect(loc)
     cursor = conn.cursor()
-    successful_entities = [i[0] for i in cursor.execute('select entity_name from search_results where not failed')]
+    successful_entities = [i[0] for i in cursor.execute(
+        'select entity_name from search_results where not failed')]
     failed_entities = set(GetAllEntities()) - set(successful_entities)
     return failed_entities
     
