7f83eb615093c6154fcca3a602ae55c36be85b23
Huichao Xue <xue.huichao@gmail.com>
2016-04-06 18:42:43 -0700

    Now using the right SQL to print out experimental results.

--- experiments_summary.py
+++ experiments_summary.py
@@ -11,11 +11,14 @@ def main():
   cursor = connection.cursor()
   latest_results = list(
       cursor.execute(
-          "select exp_id, max(timestamp), avg_map, "
-          "celebrity, movie, restaurant, tvShow from experiments group by features"))
+        "select E.exp_id, E.features, E.avg_map, "
+        "E.celebrity, E.movie, E.restaurant, E.tvShow, E.timestamp from experiments as E inner join "
+        "(select exp_id, max(timestamp) from experiments group by features) b "
+        "on E.exp_id = b.exp_id order by E.avg_map desc"))
   print tabulate.tabulate(
       latest_results,
-      headers=["ID", "Time", "MAP", "Celebrity", "Movie", "Restaurant", "tvShow"])
+      headers=["ID", "features", "MAP", "Celebrity", "Movie", "Restaurant", "tvShow", "time"])
+
 
 if __name__ == "__main__":
   main()
